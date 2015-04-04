0e3badd250c839a1d33a35ee3c031f11450f41fb
Joseph Drambarean <jdrambarean@gmail.com>
2015-04-04 15:05:41 -0400

    Made some sweeping changes to the code base and added a few new classes so that I can start to try and draw my chart.

--- HelvetiSteps/HelvetiSteps/FirstViewController.swift
+++ HelvetiSteps/HelvetiSteps/FirstViewController.swift
@@ -17,41 +17,13 @@ class FirstViewController: UIViewController, LineChartDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        requestHealthKitAuthorization()
-        
-        var views: Dictionary<String, AnyObject> = [:]
-        
-        label.text = "..."
-        label.setTranslatesAutoresizingMaskIntoConstraints(false)
-        label.textAlignment = NSTextAlignment.Center
-        self.view.addSubview(label)
-        views["label"] = label
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[label]-|", options: nil, metrics: nil, views: views))
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-350-[label]", options: nil, metrics: nil, views: views))
-        
-        var data: Array<CGFloat> = [3, 4, 9, 11, 13, 15]
-        var data2: Array<CGFloat> = [1, 3, 5, 13, 17, 20]
-        
-        lineChart = LineChart()
-        lineChart!.areaUnderLinesVisible = true
-        lineChart!.addLine(data)
-        lineChart!.addLine(data2)
-        lineChart!.setTranslatesAutoresizingMaskIntoConstraints(false)
-        lineChart!.delegate = self
-        self.view.addSubview(lineChart!)
-        views["chart"] = lineChart
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[chart]-|", options: nil, metrics: nil, views: views))
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[label]-[chart(==200)]", options: nil, metrics: nil, views: views))
-        
-        var delta: Int64 = 4 * Int64(NSEC_PER_SEC)
-        var time = dispatch_time(DISPATCH_TIME_NOW, delta)
-        
-        dispatch_after(time, dispatch_get_main_queue(), {
-            self.lineChart!.clear()
-            self.lineChart!.addLine(data2)
-        });
+        drawChart()
     }
 
+    override func viewDidAppear(animated: Bool) {
+        requestHealthKitAuthorization()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -65,6 +37,10 @@ class FirstViewController: UIViewController, LineChartDelegate {
 
     var steps = [HKQuantitySample]()
     
+    var stepsForChart = [HKQuantitySample]()
+    
+    var stepsInFloat = [CGFloat]()
+    
     var label = UILabel()
     var lineChart: LineChart?
     
@@ -75,10 +51,11 @@ class FirstViewController: UIViewController, LineChartDelegate {
     let stepsUnit = HKUnit.countUnit()
     
     func requestHealthKitAuthorization() {
-        let dataTypesToRead = NSSet(objects: healthKitManager.stepsCount)
+        let dataTypesToRead = NSSet(objects: healthKitManager.stepsCount, healthKitManager.distanceCount)
         healthKitManager.healthStore?.requestAuthorizationToShareTypes(nil, readTypes: dataTypesToRead, completion: { [unowned self] (success, error) in
             if success {
                 self.queryStepsSum()
+                //self.querySteps()
             } else {
                 println(error.description)
             }
@@ -105,8 +82,13 @@ class FirstViewController: UIViewController, LineChartDelegate {
         label.text = "x: \(x)     y: \(yValues)"
     }
     
+    func valueConversion() {
+        var floatArray = self.stepsForChart.map({CGFloat($0.quantity.doubleValueForUnit(self.healthKitManager.stepsUnit))})
+        self.stepsInFloat = floatArray
+    }
+    
     func queryStepsSum() {
-        activity.startAnimating()
+        self.activity.startAnimating()
         let sumOption = HKStatisticsOptions.CumulativeSum
         let startDate = NSDate().dateByRemovingTime()
         let endDate = NSDate()
@@ -135,6 +117,51 @@ class FirstViewController: UIViewController, LineChartDelegate {
             }
         }
         healthKitManager.healthStore?.executeQuery(statisticsSumQuery)
+    }
+    
+    func querySteps() {
+        let sampleQuery = HKSampleQuery(sampleType: healthKitManager.stepsCount, predicate: nil, limit: 100, sortDescriptors: nil)
+            { [unowned self] (query,results, error) in
+                if let results = results as? [HKQuantitySample] {
+                    self.stepsForChart = results
+                    //self.drawChart()
+                }
+        }
+        healthKitManager.healthStore?.executeQuery(sampleQuery)
+}
+    
+    func drawChart() {
+        var views: Dictionary<String, AnyObject> = [:]
+        
+        self.label.text = "..."
+        self.label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.label.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(self.label)
+        views["label"] = self.label
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[label]-|", options: nil, metrics: nil, views: views))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-350-[label]", options: nil, metrics: nil, views: views))
+        
+        var data: Array<CGFloat> = [1, 2, 3, 12, 23, 44, 2, 19, 102, 23]
+        //var data2: Array<CGFloat> = [1, 3, 5, 13, 17, 20]
+        
+        self.lineChart = LineChart()
+        self.lineChart!.areaUnderLinesVisible = true
+        self.lineChart!.addLine(data)
+        //lineChart!.addLine(data2)
+        self.lineChart!.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.lineChart!.delegate = self
+        self.view.addSubview(self.lineChart!)
+        views["chart"] = self.lineChart
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[chart]-|", options: nil, metrics: nil, views: views))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[label]-[chart(==200)]", options: nil, metrics: nil, views: views))
+        
+        var delta: Int64 = 4 * Int64(NSEC_PER_SEC)
+        var time = dispatch_time(DISPATCH_TIME_NOW, delta)
+        
+        dispatch_after(time, dispatch_get_main_queue(), {
+            self.lineChart!.clear()
+            //self.lineChart!.addLine(data2)
+        });
     }
     
 }

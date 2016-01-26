804f5c51f58ab01355e023f1653e2183c03c126b
Joseph Drambarean <joseph.drambarean@capitalone.com>
2016-01-26 12:06:10 -0500

    Huge Feature update
    Added Apple Watch and Today Widget Functionality

---
+++ HelvetiSteps/HelvetiSteps Watch Extension/InterfaceController.swift
@@ -0,0 +1,86 @@
+//
+//  InterfaceController.swift
+//  HelvetiSteps Watch Extension
+//
+//  Created by Drambarean, Joseph on 1/26/16.
+//  Copyright © 2016 Joseph Drambarean. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import HealthKit
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        requestHealthKitAuthorization()
+        
+            }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        
+        requestHealthKitAuthorization()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    let healthStore: HKHealthStore? = {
+        if HKHealthStore.isHealthDataAvailable() {
+            return HKHealthStore()
+        } else {
+            return nil
+        }
+    }()
+    
+    
+    @IBOutlet var stepsCountLabel: WKInterfaceLabel!
+    
+    let stepsCount = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+    let distanceCount = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)
+    let heartRate = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate)
+    let activeCalories = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned)
+    let stepsUnit = HKUnit.countUnit()
+    let distanceUnit = HKUnit(fromString: "mi")
+    let heartRateUnit = HKUnit(fromString: "count/min")
+    let activeCaloriesUnit = HKUnit(fromString: "kcal")
+    var steps = [HKQuantitySample]()
+    
+    func requestHealthKitAuthorization() {
+        let dataTypesToRead = NSSet(objects: stepsCount!, distanceCount!)
+        healthStore?.requestAuthorizationToShareTypes(nil, readTypes: dataTypesToRead as NSSet as? Set<HKObjectType>, completion: { [unowned self] (success, error) in
+            if success {
+                self.queryStepsSum()
+            } else {
+                print(error!.description)
+            }
+            })
+    }
+    
+    func queryStepsSum() {
+        let sumOption = HKStatisticsOptions.CumulativeSum
+        let startDate = NSDate().dateByRemovingTime()
+        let endDate = NSDate()
+        let predicate = HKQuery.predicateForSamplesWithStartDate(startDate, endDate: endDate, options: [])
+        let statisticsSumQuery = HKStatisticsQuery(quantityType: self.stepsCount!, quantitySamplePredicate: predicate, options: sumOption) { [unowned self] (query, result, error) in
+            if let sumQuantity = result?.sumQuantity() {
+                dispatch_async(dispatch_get_main_queue(), {
+                    let numberOfSteps = Int(sumQuantity.doubleValueForUnit(self.stepsUnit))
+                    self.stepsCountLabel.setText("\(numberOfSteps)")
+                    print("success")
+                })
+            }
+            
+        }
+        self.healthStore?.executeQuery(statisticsSumQuery)
+    }
+    
+    
+}
---
+++ HelvetiSteps/HelvetiStepsWidget/TodayViewController.swift
@@ -0,0 +1,85 @@
+//
+//  TodayViewController.swift
+//  HelvetiStepsWidget
+//
+//  Created by Drambarean, Joseph on 1/26/16.
+//  Copyright © 2016 Joseph Drambarean. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+import HealthKit
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    let healthKitManager = HealthKitManager.sharedInstance
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateStepsCountLabel()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        requestHealthKitAuthorization()
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
+        requestHealthKitAuthorization()
+        
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NewData)
+    }
+    
+    @IBOutlet weak var stepsCountLabel: UILabel!
+    
+    func updateStepsCountLabel() {
+        requestHealthKitAuthorization()
+    }
+    
+    func requestHealthKitAuthorization() {
+        let dataTypesToRead = NSSet(objects: healthKitManager.stepsCount!, healthKitManager.distanceCount!)
+        healthKitManager.healthStore?.requestAuthorizationToShareTypes(nil, readTypes: dataTypesToRead as NSSet as? Set<HKObjectType>, completion: { [unowned self] (success, error) in
+            if success {
+                self.queryStepsSum()
+                //self.querySteps()
+            } else {
+                print(error!.description)
+            }
+            })
+    }
+    
+    func queryStepsSum() {
+        let sumOption = HKStatisticsOptions.CumulativeSum
+        let startDate = NSDate().dateByRemovingTime()
+        let endDate = NSDate()
+        let predicate = HKQuery.predicateForSamplesWithStartDate(startDate, endDate: endDate, options: [])
+        let statisticsSumQuery = HKStatisticsQuery(quantityType: healthKitManager.stepsCount!, quantitySamplePredicate: predicate, options: sumOption) { [unowned self] (query, result, error) in
+            if let sumQuantity = result?.sumQuantity() {
+                dispatch_async(dispatch_get_main_queue(), {
+                    let numberOfSteps = Int(sumQuantity.doubleValueForUnit(self.healthKitManager.stepsUnit))
+                    self.stepsCountLabel.text = "\(numberOfSteps)"
+                })
+            }
+            
+        }
+        healthKitManager.healthStore?.executeQuery(statisticsSumQuery)
+    }
+    
+}
+
+
+extension NSDate {
+    func dateByRemovingTime() -> NSDate {
+        let flags: NSCalendarUnit = [.NSDayCalendarUnit, .NSMonthCalendarUnit, .NSYearCalendarUnit]
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(flags, fromDate: self)
+        return calendar.dateFromComponents(components)!
+    }
+}
+
